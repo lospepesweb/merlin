29a4f57f0c41398a7af30bc0815bf8d0f2ed3760
Luciano Penafiel <lucianopenafie@gmail.com>
2018-01-24 18:36:40 -0300

    agreo favicon y terminamos la web, salu

--- maqueta/index.php
+++ maqueta/index.php
@@ -8,6 +8,14 @@
 	<title>Merlín Estudio</title>
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
+	<!-- FAVICON -->
+	
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/manifest.json">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="theme-color" content="#ffffff">
 
 
 	<!-- METATAGS -->
